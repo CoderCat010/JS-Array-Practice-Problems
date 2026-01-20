1b0060034f331a46a6c6ab6330d58f30cd9cd1d4
CoderCat010 <nusraumme@gmail.com>
2026-01-20 17:25:23 +0600

    added practice-2

--- array-basic-practice-problems.js
+++ array-basic-practice-problems.js
@@ -1,3 +1,10 @@
 //-----> Print all elements of an array.
-let arrElm = [1, 2, 3, 4, 5];
-console.log(arrElm);
+// let arrElm = [1, 2, 3, 4, 5];
+// console.log(arrElm);
+
+
+
+//-----> Print array in reverse order.
+// let arrElm = [1, 2, 3, 4, 5]; 
+// let inReverse = arrElm.toReversed();
+// console.log(inReverse);
