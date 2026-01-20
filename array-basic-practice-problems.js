aa8b2485285604a652f95c56f5f0f37bfb71ad96
CoderCat010 <nusraumme@gmail.com>
2026-01-20 17:31:13 +0600

    added practice-3

--- array-basic-practice-problems.js
+++ array-basic-practice-problems.js
@@ -8,3 +8,16 @@
 // let arrElm = [1, 2, 3, 4, 5]; 
 // let inReverse = arrElm.toReversed();
 // console.log(inReverse);
+
+
+
+//-----> Find the length of an array (without .length) and (with .length).
+let arrElm = [1, 2, 3, 4, 5]; 
+let count = 0;
+
+// for(let elements = 0; elements < arrElm.length; elements++){
+//     count++;
+// }
+// console.log(count);
+// or 
+// console.log(arrElm.length);
