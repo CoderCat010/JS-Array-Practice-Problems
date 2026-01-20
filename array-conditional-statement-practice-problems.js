835a46cdb41dc2f6d7b4c167e9a660b687746b3d
CoderCat010 <nusraumme@gmail.com>
2026-01-20 17:49:12 +0600

    added practice-4

--- array-conditional-statement-practice-problems.js
+++ array-conditional-statement-practice-problems.js
@@ -7,3 +7,11 @@
 // let cleanEmail = userInput.trim().toLowerCase();
 
 // console.log(cleanEmail);
+
+
+
+//-----> Format an 11-digit phone number to international format.
+/*
+    * Input example: "01712345678"
+    * Output format: "+880-171-234-5678"
+*/
