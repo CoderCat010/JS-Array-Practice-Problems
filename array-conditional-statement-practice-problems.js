aaf8af52bd9fddb9ef3afcfa3d17d13a6ec0b207
CoderCat010 <nusraumme@gmail.com>
2026-01-20 18:09:32 +0600

    added practice-5

--- array-conditional-statement-practice-problems.js
+++ array-conditional-statement-practice-problems.js
@@ -15,3 +15,10 @@
     * Input example: "01712345678"
     * Output format: "+880-171-234-5678"
 */
+let userInput = '1734678998';
+let part1 = userInput.slice(0,3);
+let part2 = userInput.slice(3,6);
+let part3 = userInput.slice(6);
+let formatedVersion = '880'+'-'+part1+"-"+part2+"-"+part3;
+
+console.log(formatedVersion);
