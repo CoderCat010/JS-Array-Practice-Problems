eb789aac1ab8a790b93394fd56472996c72126fa
CoderCat010 <nusraumme@gmail.com>
2026-01-21 08:34:42 +0600

    added practice-10

--- array-object-practice-problems.js
+++ array-object-practice-problems.js
@@ -16,6 +16,24 @@
 
 
 
+//-----> Find employee with highest salary.
+/*
+    * input: get 3 array like objects
+    * output: Return that employee with highest salary.
+ */
+let employees = [
+    {salary: 12000}, {salary: 13000}, {salary: 14000}
+]
+
+// if(employees[0].salary > employees[1].salary && employees[0].salary > employees[2].salary){
+//     console.log({'the highest paid employee is': employees[0].salary});
+// }else if(employees[1].salary > employees[0].salary && employees[1].salary > employees[2].salary){
+//     console.log({'the highest paid employee is': employees[1].salary});
+// }else{
+//     console.log({'the highest paid employee is': employees[2].salary});
+// }
+
+
 
 
 
