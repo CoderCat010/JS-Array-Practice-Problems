5261e30cad123e21a3498bc1fd35a99f770e2262
CoderCat010 <nusraumme@gmail.com>
2026-01-21 08:14:47 +0600

    added practice-9

--- array-conditional-statement-practice-problems.js
+++ array-conditional-statement-practice-problems.js
@@ -19,23 +19,6 @@
 
 
 
-//-----> Given an array of numbers, count how many are even and how many are odd.
-/*
-    * Input: [1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 10]
-    * Output: { even: 6, odd: 5 }
-*/
-// let arrElm = [1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 10];
-// let countEvenNum = 0;
-// let countOddNum = 0;
-
-// for(let i = 0; i < arrElm.length; i++){
-//     if(arrElm[i] % 2 == 0){
-//         countEvenNum++;
-//     }else{
-//         countOddNum ++;
-//     }
-// }
-// console.log({even: countEvenNum, odd: countOddNum});
 
 
 
---
+++ array-object-practice-problems.js
@@ -0,0 +1,21 @@
+//-----> Given an array of employee objects.
+/*
+    * input: [
+         { name: "A", salary: 20000 },
+         { name: "B", salary: 30000 }
+    ]
+    * output: Return total salary.
+*/
+// let employees = [
+//     { name: "A", salary: 20000 },
+//     { name: "B", salary: 30000 }
+// ];
+
+// let totalSalary = employees[0].salary + employees[1].salary;
+// console.log(totalSalary);
+
+
+
+
+
+
